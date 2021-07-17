0acb6c7385a727e295cf7894af8b2237a7621b0f
yousef33i <78238700+yousef33i@users.noreply.github.com>
2021-07-17 21:47:05 +0300

    Add files via upload

---
+++ lec28.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class lec28 {
+
+	public static void main(String[] args) {
+		int a =Integer.parseInt(JOptionPane.showInputDialog("«œŒ· —ﬁ„"));
+		System.out.println(Math.sqrt(a));
+        System.out.println(Math.abs(a));
+        System.out.println(Math.pow(a,3));
+         
+	}
+
+}
